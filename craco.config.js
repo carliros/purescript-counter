1262de79f3afdc17c9fb7d13eb8b5e1fe491f644
Carlos Gómez <carliros.g@gmail.com>
2020-08-22 16:10:19 -0400

    setup craco

---
+++ craco.config.js
@@ -0,0 +1,12 @@
+const cracoPureScriptLoader = require("craco-purescript-loader");   
+module.exports = {
+    plugins: [
+        {
+            plugin: cracoPureScriptLoader,
+            options: {
+                spago: true,
+                pscIde: true,
+            },
+        },
+    ],
+};
